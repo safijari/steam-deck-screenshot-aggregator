fa8f2f1cb9c580a71e73cc9adda365c44537c08f
Jariullah <safijari@isu.edu>
2022-08-21 18:22:09 -0400

    Add copy option

--- sdsa.py
+++ sdsa.py
@@ -4,9 +4,14 @@ from pathlib import Path
 import json
 import os
 import subprocess
+import sys
+import shutil
 
 
 def main():
+    do_copy = False
+    if len(sys.argv) > 1 and sys.argv[1] == "copy":
+        do_copy = True
     path = Path.home() / ".local/share/Steam/userdata"
     files = list(path.glob("**/screenshots/*.jpg"))
 
@@ -14,7 +19,7 @@ def main():
         "curl https://api.steampowered.com/ISteamApps/GetAppList/v2/ > /tmp/appidmap.json",
         shell=True,
         check=True,
-        capture_output=True
+        capture_output=True,
     )
 
     id_map = {
@@ -29,8 +34,13 @@ def main():
         name = id_map.get(appid, appid)
         final_path = dump_folder / name / f.name
         final_path.parent.mkdir(parents=True, exist_ok=True)
-        if not final_path.exists():
-            os.symlink(f, final_path)
+        if not do_copy:
+            if not final_path.exists():
+                os.symlink(f, final_path)
+        else:
+            if final_path.is_symlink():
+                final_path.unlink()
+            shutil.copy(f, final_path, follow_symlinks=False)
 
 
 if __name__ == "__main__":
